6eda6cb5696b8e14bd9a9bfbc4c7264b4a044893
oluwadareseyi <oluwadareseyii@gmail.com>
2021-01-25 21:16:51 +0100

    build footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="c-footer container">
+    <div class="c-footer__inner">
+      <p class="c-footer__copy">Copyright 2019. Flutterwave inc</p>
+      <div class="c-footer__legal">
+        <p>Terms & Conditions</p>
+        <p>Privacy Policy</p>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ components/AppNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="c-nav container">
+    <logo />
+    <div class="c-nav__links">
+      <nuxt-link to="/retrieve">I didn't get my tickets</nuxt-link>
+      <button class="c-button c-button--yellow">Create your own event</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+import Logo from "./Logo.vue";
+export default {
+  components: { Logo }
+};
+</script>
+
+<style lang="scss" scoped></style>
