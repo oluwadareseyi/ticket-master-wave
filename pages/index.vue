457e109c5d086c3436f758e3063e9b3309479972
oluwadareseyi <oluwadareseyii@gmail.com>
2021-01-25 22:12:24 +0100

    build landing page layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,83 @@
 <template>
-  <div class="c-home">Home</div>
+  <main class="c-landing container">
+    <h1 class="c-landing__title">The best events happening now.</h1>
+    <section class="c-landing__events">
+      <nuxt-link to="/" class="c-event">
+        <div class="c-event__image">
+          <img
+            src="https://res.cloudinary.com/dmwfd0zhh/image/upload/v1611606590/flutterwave/Event_image_ifefam.png"
+            alt="The Nathan Cole Experience"
+          />
+        </div>
+        <div class="c-event__details">
+          <p class="c-event__date">8th February 2019</p>
+          <h3 class="c-event__title">The Nathan Cole Experience</h3>
+          <p class="c-event__cost">N5000 – N2,000,000</p>
+        </div>
+      </nuxt-link>
+
+      <nuxt-link to="/" class="c-event">
+        <div class="c-event__image">
+          <img
+            src="https://res.cloudinary.com/dmwfd0zhh/image/upload/v1611606590/flutterwave/Event_image_ifefam.png"
+            alt="The Nathan Cole Experience"
+          />
+        </div>
+        <div class="c-event__details">
+          <p class="c-event__date">8th February 2019</p>
+          <h3 class="c-event__title">The Nathan Cole Experience</h3>
+          <p class="c-event__cost">N5000 – N2,000,000</p>
+        </div>
+      </nuxt-link>
+
+      <nuxt-link to="/" class="c-event">
+        <div class="c-event__image">
+          <img
+            src="https://res.cloudinary.com/dmwfd0zhh/image/upload/v1611606590/flutterwave/Event_image_ifefam.png"
+            alt="The Nathan Cole Experience"
+          />
+        </div>
+        <div class="c-event__details">
+          <p class="c-event__date">8th February 2019</p>
+          <h3 class="c-event__title">The Nathan Cole Experience</h3>
+          <p class="c-event__cost">N5000 – N2,000,000</p>
+        </div>
+      </nuxt-link>
+
+      <nuxt-link to="/" class="c-event">
+        <div class="c-event__image">
+          <img
+            src="https://res.cloudinary.com/dmwfd0zhh/image/upload/v1611606590/flutterwave/Event_image_ifefam.png"
+            alt="The Nathan Cole Experience"
+          />
+        </div>
+        <div class="c-event__details">
+          <p class="c-event__date">8th February 2019</p>
+          <h3 class="c-event__title">The Nathan Cole Experience</h3>
+          <p class="c-event__cost">N5000 – N2,000,000</p>
+        </div>
+      </nuxt-link>
+
+      <nuxt-link to="/" class="c-event">
+        <div class="c-event__image">
+          <img
+            src="https://res.cloudinary.com/dmwfd0zhh/image/upload/v1611606590/flutterwave/Event_image_ifefam.png"
+            alt="The Nathan Cole Experience"
+          />
+        </div>
+        <div class="c-event__details">
+          <p class="c-event__date">8th February 2019</p>
+          <h3 class="c-event__title">The Nathan Cole Experience</h3>
+          <p class="c-event__cost">N5000 – N2,000,000</p>
+        </div>
+      </nuxt-link>
+    </section>
+    <div class="c-landing__pagination">
+      <button class="c-button c-button--yellow">
+        Load More
+      </button>
+    </div>
+  </main>
 </template>
 
 <script>
