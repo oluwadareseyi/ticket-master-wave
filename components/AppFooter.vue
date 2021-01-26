b6475b968726577e81e3ccc35a469ee8e89aec3a
oluwadareseyi <oluwadareseyii@gmail.com>
2021-01-26 07:57:56 +0100

    build event details layout

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="c-footer container">
+  <footer class="c-footer">
     <div class="c-footer__inner">
       <p class="c-footer__copy">Copyright 2019. Flutterwave inc</p>
       <div class="c-footer__legal">
